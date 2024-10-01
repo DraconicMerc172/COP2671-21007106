7d38837209e80a0f7996642c1e1f165f19cf5f2d
DraconicMerc172 <justin.trebour@gmail.com>
2024-10-01 10:13:10 -0400

    Finishing up Step 5.2

--- Unit_5-User Interface/Prototype 5/Assets/Scripts/GameManager.cs
+++ Unit_5-User Interface/Prototype 5/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,11 +10,19 @@ public class GameManager : MonoBehaviour
 
     private float spawnRate = 1.0f;
 
+    private int score;
+    
+    public TextMeshProUGUI scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
 
         StartCoroutine(SpawnTarget());
+
+        score = 0;
+
+        UpdateScore(0);
         
     }
 
@@ -21,6 +30,15 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void UpdateScore(int scoreToAdd) 
+    {
+
+        score += scoreToAdd;
+
+        scoreText.text = "Score: " + score;
+
     }
 
     IEnumerator SpawnTarget() 
--- Unit_5-User Interface/Prototype 5/Assets/Scripts/Targets.cs
+++ Unit_5-User Interface/Prototype 5/Assets/Scripts/Targets.cs
@@ -18,6 +18,12 @@ public class NewBehaviourScript : MonoBehaviour
 
     private float ySpawnPos = -6;
 
+    private GameManager gameManager;
+
+    public int pointValue;
+
+    public ParticleSystem explosionParticle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +35,8 @@ public class NewBehaviourScript : MonoBehaviour
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
         transform.position = RandomSpawnPos();
+
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
     }
 
@@ -42,6 +50,10 @@ public class NewBehaviourScript : MonoBehaviour
     {
 
         Destroy(gameObject);
+
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+
+        gameManager.UpdateScore(pointValue);
 
     }
 
